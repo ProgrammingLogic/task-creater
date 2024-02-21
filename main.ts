9096fa3c8f58072369c956c5d46d97d0d24b0500
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-20 23:24:08 -0600

    Refactor CreateTaskModal constructor and onSubmit callback

--- main.ts
+++ main.ts
@@ -28,7 +28,9 @@ export default class TaskCreater extends Plugin {
 		await this.loadSettings();
 
 		const createTaskRibbonIcon = this.addRibbonIcon("list-checks","Create Task", (evt: MouseEvent) => {
-			new CreateTaskModal(this.app).open();
+			new CreateTaskModal(this.app, (task) => {
+
+			}).open();
 		});
 	}
 
@@ -47,13 +49,67 @@ export default class TaskCreater extends Plugin {
 
 
 class CreateTaskModal extends Modal {
-	constructor(app: App) {
+	task: {
+		name: string;
+		description: string;
+		dueDate: string;
+		createdDate: string;
+	};
+
+	
+	onSubmit: (task: object) => void;
+
+	// returns task object when submitted???
+	constructor(app: App, onSubmit: (task: object) => void) {
 		super(app);
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText("Hello world!");
+		
+		contentEl.createEl("h1", {text: "Create your task!" });
+
+
+		// Get the name of the task
+		new Setting(contentEl)
+			.setName("Task Name")
+			.addText((text) => 
+				text.onChange((value) => {
+					this.task.name = value;
+				}));
+
+		// Get the description of the task
+		new Setting(contentEl)
+			.setName("Task Description")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.task.description = value;
+				}));
+
+		// Get the due date of the task
+		new Setting(contentEl)
+			.setName("Due Date")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.task.dueDate = value;
+				}));
+
+		// Get the created date of the task
+		new Setting(contentEl)
+			.setName("Created Date")
+			.addText((text) =>
+				text.onChange((value) => {
+					this.task.createdDate = value;
+				}));
+
+		new Setting(contentEl)
+			.setName("Submit")
+			.addButton((button) => 
+				button.setButtonText("Submit")
+				.onClick(() => {
+					this.close();
+					this.onSubmit(this.task);
+				}));
 	}
 
 
