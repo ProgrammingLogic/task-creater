d6d872e86681e72ceb3fb4c91426d6a4d5e7d346
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-20 23:30:39 -0600

    Add TaskCreaterSettingsTab class to main.ts

--- main.ts
+++ main.ts
@@ -118,3 +118,32 @@ class CreateTaskModal extends Modal {
 		contentEl.empty();
 	}
 }
+
+
+class TaskCreaterSettingsTab extends PluginSettingTab {
+	plugin: TaskCreater;
+
+	constructor(app: App, plugin: TaskCreater) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Tasks Directory")
+			.setDesc("Where created tasks will be stored")
+			.addText(text => text
+				.setPlaceholder("Enter the tasks directory")
+				.setValue(this.plugin.settings.TasksDirectory)
+				.onChange(async (value) => {
+					this.plugin.settings.TasksDirectory = value;
+					await this.plugin.saveSettings();
+				})
+			)
+	}
+}
