37cd7a2d93290d658e76f3b950ff85bf3f80fb95
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-20 23:04:47 -0600

    Add create task button and modal

--- main.ts
+++ main.ts
@@ -24,7 +24,11 @@ export default class TaskCreater extends Plugin {
 	settings: TaskCreaterSettings;
 
 	async onload() {
-		
+		await this.loadSettings();
+
+		const createTaskRibbonIcon = this.addRibbonIcon("list-checks","Create Task", (evt: MouseEvent) => {
+			new Notice("Create task button has been pressed!");
+		});
 	}
 
 	onunload() {
@@ -37,5 +41,23 @@ export default class TaskCreater extends Plugin {
 
 	async saveSettings() {
 		
+	}
+}
+
+
+class CreateTaskModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		const {contentEl} = this;
+		contentEl.setText("Hello world!");
+	}
+
+
+	onClose() {
+		const {contentEl} = this;
+		contentEl.empty();
 	}
 }
