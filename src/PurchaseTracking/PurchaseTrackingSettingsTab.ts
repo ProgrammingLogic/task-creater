c2447a946769b0a9bcf1ffd052025641c1a257aa
jonathyn.stiverson <jstiverson>
2024-02-23 13:42:51 -0600

    Implement boilerplate for PurchaseTrackingSettingsTab

---
+++ src/PurchaseTracking/PurchaseTrackingSettingsTab.ts
@@ -0,0 +1,19 @@
+import { App, PluginSettingTab, Setting } from "obsidian";
+import { PurchaseTracking } from "PurchaseTracking";
+import { PersonalPlugin } from "../main";
+
+
+export class PurchaseTrackingSettingsTab extends PluginSettingTab {
+    plugin: PersonalPlugin;
+
+    constructor(app: App, plugin: PersonalPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+
+    display(): void {
+        // todo
+        // add settings options for PurchaseTracking
+    }
+}
