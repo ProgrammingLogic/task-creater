beb95d824156c2c24a24c41d72336e7b3f7c08fe
jonathyn.stiverson <jstiverson>
2024-02-23 13:50:03 -0600

    Add base for ShoppingCart.ts

---
+++ src/PurchaseTracking/ShoppingCart.ts
@@ -0,0 +1,17 @@
+import App from "obsidian";
+import { Purchase } from "./Purchase";
+import PersonalPlugin from "../main";
+import { PersonalPluginSettings } from "src/settings";
+
+
+export class PurchaseTracking {
+    app: App;
+    plugin: PersonalPlugin;
+    settings: PersonalPluginSettings;
+    PurchasedItems: Purchase[];
+
+
+    constructor(app: App, plugin: PersonalPlugin) {
+
+    }
+}
