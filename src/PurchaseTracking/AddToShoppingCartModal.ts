5ffe1bd23c537eec41deadcd72264d75821897c3
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-24 00:57:40 -0600

    Add item to shopping cart modal with settings and functionality

--- src/PurchaseTracking/AddToShoppingCartModal.ts
+++ src/PurchaseTracking/AddToShoppingCartModal.ts
@@ -1,22 +1,144 @@
-import { App, Modal, Notice, Setting } from "obsidian";
+import { App, ButtonComponent, Component, Modal, Notice, Setting } from "obsidian";
 import { Purchase } from "./Purchase";
 
 
 
 export class AddToShoppingCartModal extends Modal {
+    purchase: Purchase;
+
+
     constructor(app: App) {
         super(app);
     }
 
 
     onOpen() {
+        this.purchase = new Purchase(this.app);
         const {contentEl} = this;
-        contentEl.setText("Shopping Cart Menu!");
+        contentEl.setText("Add Item to Cart");
+
+
+        new Setting(contentEl)
+            .setName("Item Name")
+            .setDesc("The name of the item you want to add to the shopping cart")
+            .addText(text => text
+                .setPlaceholder("Enter the item name")
+                .onChange(value => {
+                    this.purchase.setName(value);
+                }));
+
+
+        new Setting(contentEl)
+            .setName("Item Description")
+            .setDesc("A description of the item you want to add to the shopping cart")
+            .addTextArea(text => text
+                .setPlaceholder("Enter the item description")
+                .onChange(value => {
+                    this.purchase.setDescription(value);
+                }));
+
+
+        new Setting(contentEl)
+            .setName("Item Price")
+            .setDesc("The price of the item you want to add to the shopping cart")
+            .addText(text => text
+                .setPlaceholder("Enter the item price")
+                .onChange(value => {
+                    this.purchase.setPrice(Number(value));
+                }));
+
+
+        new Setting(contentEl)
+            .setName("Item Rating")
+            .setDesc("The rating of how much you want the item you want to add to the shopping cart")
+            .addText(text => text
+                .setPlaceholder("Enter the item rating")
+                .onChange(value => {
+                    this.purchase.setRating(Number(value));
+                }));
+
+
+        new Setting(contentEl)
+            .setName("Want or Need?")
+            .setDesc("Do you want the item or do you need it?")
+            .addDropdown(dropdown => {
+                dropdown.addOption("Want", "want");
+                dropdown.addOption("Need", "need");
+
+                dropdown.onChange(value => {
+                    if (value === "want") {
+                        this.purchase.setWant(true);
+                        this.purchase.setNeed(false);
+                    }
+                    else if (value === "need") {
+                        this.purchase.setNeed(true);
+                        this.purchase.setWant(false);
+                    }
+                })
+            });
+
+
+        new Setting(contentEl)
+            .setName("Due Date")
+            .setDesc("When the item should be bought by")
+            .addText(text => text
+                .setPlaceholder("Enter the due date")
+                .onChange(value => {
+                    this.purchase.setDueDate(new Date(value));
+                }));
+
+
+        new Setting(contentEl)
+            .setName("Item URL")
+            .setDesc("The URL of the item you want to add to the shopping cart")
+            .addText(text => text
+                .setPlaceholder("Enter the item URL")
+                .onChange(value => {
+                    this.purchase.setURL(value);
+                }));
+
+
+        new Setting(contentEl)
+            .setName("Item Note Path")
+            .setDesc("The path to the note containing the item you want to add to the shopping cart")
+            .addText(text => text
+                .setPlaceholder("Enter the item note path")
+                .onChange(value => {
+                    this.purchase.setNotePath(value);
+                }));
+
+
+        new Setting(contentEl)
+            .setName("Item Tags")
+            .setDesc("The tags associated with the item you want to add to the shopping cart")
+            .addTextArea(text => text
+                .setPlaceholder("Enter the item tags")
+                .onChange(value => {
+                    const tags = value.split(",");
+                    for (let tag of tags) {
+                        this.purchase.addTag(tag);
+                    }
+                }));
+        
+
+        new Setting(contentEl)
+            .addButton(button => {
+                button.setButtonText("Add to Cart")
+                    .onClick(async () => {
+                        await this.addItemToCart();
+                    });
+            });
     }
 
 
     onClose() {
         const {contentEl} = this;
         contentEl.empty();
+    }
+
+
+    async addItemToCart() {
+        const purchase = new Purchase(this.app);
+        console.log(`Adding ${this.purchase.Name} to the shopping cart`);
     }
 }
