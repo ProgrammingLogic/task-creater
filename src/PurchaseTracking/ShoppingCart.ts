48376f3cc15820d7a6c3cc1cf14550d717197558
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-23 23:11:23 -0600

    Add purchase tracking methods to ShoppingCart class

--- src/PurchaseTracking/ShoppingCart.ts
+++ src/PurchaseTracking/ShoppingCart.ts
@@ -12,6 +12,39 @@ export class PurchaseTracking {
 
 
     constructor(app: App, plugin: PersonalPlugin) {
+        this.app = app;
+        this.plugin = plugin;
+        this.settings = this.plugin.settings;
+        this.PurchasedItems = [];
+    }
 
+
+    addPurchase(purchase: Purchase) {
+        this.PurchasedItems.push(purchase);
+    }
+
+
+    removePurchase(purchase: Purchase) {
+        this.PurchasedItems = this.PurchasedItems.filter(p => p.ID !== purchase.ID);
+    }
+
+
+    getPurchaseByID(ID: string) {
+        return this.PurchasedItems.find(p => p.ID === ID);
+    }
+
+
+    getPurchaseByName(name: string) {
+        return this.PurchasedItems.find(p => p.Name === name);
+    }
+
+
+    getPurchaseByURL(url: string) {
+        return this.PurchasedItems.find(p => p.URL === url);
+    }
+
+
+    getPurchaseByTag(tag: string) {
+        return this.PurchasedItems.filter(p => p.Tags.includes(tag));
     }
 }
