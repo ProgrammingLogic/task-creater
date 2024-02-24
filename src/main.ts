89b88e93cec803b9321583f8b1049704b0f76bcd
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-02-24 00:06:00 -0600

    Add ShoppingCart and PurchaseTracking classes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, Menu } from 'obsidian';
 import { PersonalPluginSettings, PersonalPluginSettingsTab, DEFAULT_SETTINGS } from './settings';
 import { Purchase } from 'src/PurchaseTracking/Purchase';
+import { ShoppingCart } from './PurchaseTracking/ShoppingCart';
+import { PurchaseTracking } from './PurchaseTracking/PurchaseTracking';
 
 
 export default class PersonalPlugin extends Plugin {
 	settings: PersonalPluginSettings;
+	shoppingCart: ShoppingCart;
+	purchaseTracking: PurchaseTracking;
+	
 
 	async onload() {
 		await this.loadSettings();
